f9157c9a6ea55e54b93a4aaa5146eb4af64a36d2
AB-ohi <askohi63907@gmail.com>
2023-03-15 18:38:39 +0600

    khsd

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 document.getElementById('profile-img').addEventListener('mouseover',function(){
     const aroAddStyle = document.getElementById('img-aro');
-    aroAddStyle.style.display = 'none';
+    aroAddStyle.style.marginRight = '2px';
+})
+document.getElementById('profile-img').addEventListener('mouseout',function(){
+    const aroAddStyle = document.getElementById('img-aro');
+    aroAddStyle.style.marginRight = '-190px';
 })
